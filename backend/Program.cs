ada5ed2b85725fdd8a3e3abe98b349418fbcf763
Joel Edegran <joel_edegran@hotmail.com>
2026-08-12 00:15:17 +0200

    chore(backend): initialize CarApi project

---
+++ backend/Program.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Builder;
+
+namespace CarApi;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+
+        app.MapGet("/", () => "Hello World!");
+
+        app.Run();
+    }
+}
